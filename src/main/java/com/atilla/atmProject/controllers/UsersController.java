f9e04b0de2d79258ba34992875cfc586c57e7059
Atilla BAYRAM <atillabayramn@gmail.com>
2023-09-19 14:50:43 +0300

    token eklendi

--- src/main/java/com/atilla/atmProject/controllers/UsersController.java
+++ src/main/java/com/atilla/atmProject/controllers/UsersController.java
@@ -5,7 +5,6 @@ import com.atilla.atmProject.entities.User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/users")
--- src/main/java/com/atilla/atmProject/security/AuthenticationController.java
+++ src/main/java/com/atilla/atmProject/security/AuthenticationController.java
@@ -1,20 +1,17 @@
 package com.atilla.atmProject.security;
 
 import com.atilla.atmProject.business.AuthenticationService;
-import com.atilla.atmProject.business.JwtService;
-import com.atilla.atmProject.business.RefreshTokenService;
-import com.atilla.atmProject.business.UserService;
 import com.atilla.atmProject.business.requests.RefreshRequest;
 import com.atilla.atmProject.business.requests.UserDto;
 import com.atilla.atmProject.business.requests.UserRequest;
 import com.atilla.atmProject.business.responses.UserResponse;
-import com.atilla.atmProject.entities.RefreshToken;
-import com.atilla.atmProject.entities.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
